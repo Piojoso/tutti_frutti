6de8b77438b725101f1b065669887f85bc06345c
Piojoso <work.leonel.bongiovanni@outlook.com.ar>
2026-02-23 12:28:56 -0300

    Adding base game categories

--- lib/modules/new_game/screens/new_game_page.dart
+++ lib/modules/new_game/screens/new_game_page.dart
@@ -4,36 +4,58 @@ import 'package:tutti_futti_game/modules/common/widgets/custom_widgets.dart';
 class NewGamePage extends StatelessWidget {
   const NewGamePage({super.key});
 
+  static const List<String> gameCategories = [
+    'Nombre',
+    'Ciudad',
+    'Pais',
+    'Color',
+    'Animal',
+    'Planta',
+    'Fruta',
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('New Game')),
-      body: Container(
-        decoration: BoxDecoration(color: Color(0xFFFAFAFA)),
+      body: SingleChildScrollView(
         child: Column(
           children: [
             Padding(
               padding: EdgeInsets.symmetric(horizontal: 25, vertical: 10),
               child: CustomCard(
                 fullWidth: true,
-                child: Column(
-                  children: [
-                    TextFormField(
-                      decoration: InputDecoration(
-                        labelStyle: TextStyle(
-                          fontSize: 20,
-                          fontWeight: FontWeight.w600,
-                        ),
-                        labelText: 'Nombre de la Partida',
-                      ),
+                child: TextFormField(
+                  decoration: InputDecoration(
+                    labelStyle: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.w600,
                     ),
-                  ],
+                    labelText: 'Nombre de la Partida',
+                  ),
                 ),
               ),
             ),
             Padding(
               padding: EdgeInsets.symmetric(horizontal: 25, vertical: 10),
-              child: CustomCard(child: Text('Hola Mundo')),
+              child: CustomCard(
+                fullWidth: true,
+                child: Column(
+                  children: [
+                    Text('Seleccione las categorias'),
+                    ListView.separated(
+                      shrinkWrap: true,
+                      itemCount: gameCategories.length,
+                      separatorBuilder: (_, _) => Divider(),
+                      itemBuilder: (BuildContext context, int index) {
+                        final category = gameCategories[index];
+
+                        return ListTile(title: Text(category));
+                      },
+                    ),
+                  ],
+                ),
+              ),
             ),
           ],
         ),
