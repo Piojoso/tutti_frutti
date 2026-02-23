821f8629d51ca459790a95a703f0051dff0e1d82
Piojoso <work.leonel.bongiovanni@outlook.com.ar>
2026-02-23 12:26:30 -0300

    Adding customization to custom_card

--- lib/modules/common/widgets/custom_card.dart
+++ lib/modules/common/widgets/custom_card.dart
@@ -3,16 +3,26 @@ import 'package:flutter/material.dart';
 class CustomCard extends StatelessWidget {
   final bool fullWidth;
   final Widget child;
+  final EdgeInsets? padding;
 
-  const CustomCard({this.fullWidth = false, required this.child, super.key});
+  const CustomCard({
+    this.fullWidth = false,
+    this.padding,
+    required this.child,
+    super.key,
+  });
 
   static const borderSide = BorderSide(width: 2, color: Colors.black26);
+  static const defaultPadding = EdgeInsets.symmetric(
+    horizontal: 25,
+    vertical: 10,
+  );
 
   @override
   Widget build(BuildContext context) {
     return Container(
       width: fullWidth ? double.infinity : null,
-      padding: EdgeInsets.symmetric(horizontal: 25, vertical: 10),
+      padding: padding ?? defaultPadding,
       decoration: BoxDecoration(
         color: Colors.white,
         border: Border(
